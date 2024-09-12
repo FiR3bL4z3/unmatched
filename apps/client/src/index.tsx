cdd491cea468ce1470d5088d17ee337ccc90c11a
FiR3bL4z3 <horvathb0222@gmail.com>
2024-09-13 00:55:47 +0200

    chore: remove error from index.tsx

--- apps/client/src/index.tsx
+++ apps/client/src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./app";
 
-const a: number = "ASD";
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <App />
