dfba1e51a2003d9db33ba31af78327392881bf6b
blaze <horvathb0222@gmail.com>
2024-10-06 00:02:34 +0200

    refactor: [UNM/WEB-26] add openapi schema generation

--- apps/client/src/client.ts
+++ apps/client/src/client.ts
@@ -1,4 +1,13 @@
 import { hc } from "hono/client";
 import type { AppType } from "../../api/src/app";
+import createClient from "openapi-fetch";
+import { paths } from "./generated/api/v1";
 
 export const client = hc<AppType>("http://localhost:3000");
+
+export const openApiClient = createClient<paths>({
+    baseUrl: "http://localhost:3000",
+});
+
+openApiClient.GET("/health-check");
+openApiClient.GET("/players");
--- apps/client/src/routes/home.tsx
+++ apps/client/src/routes/home.tsx
@@ -1,4 +1,41 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { openApiClient } from "../client";
+import { APIError } from "../utils/api-error";
+
+const loadData = async () => {
+    const response = await openApiClient.GET("/players");
+
+    const json = response.data!;
+
+    if (!json.ok) {
+        throw new APIError(json);
+    }
+
+    return json.data;
+};
+
+const submitData = async () => {
+    const response = await openApiClient.POST("/players", {
+        body: {
+            name: "Jani",
+        },
+    });
+
+    return response.data;
+};
+
 export default function Page() {
+    const { data, isLoading, error, refetch } = useQuery({
+        queryKey: ["players"],
+        queryFn: loadData,
+    });
+    const { mutate } = useMutation({
+        mutationFn: submitData,
+        onSuccess: () => {
+            refetch();
+        },
+    });
+
     return (
         <div className="container mx-auto px-4">
             <h1 className="text-4xl font-bold text-center mt-8">
@@ -7,6 +44,13 @@ export default function Page() {
             <p className="text-lg text-center mt-4">
                 This is the home page of the app.
             </p>
+            <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8"
+                onClick={() => mutate()}
+            >
+                Create player
+            </button>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
     );
 }
